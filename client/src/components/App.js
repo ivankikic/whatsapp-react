c844e5f9fe3ff240a7a3f7b3568d6a59b8f3e9e2
Ivan Kikic <ivankikic49@gmail.com>
2023-10-10 15:49:38 +0200

    Add login page

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import useLocalStorage from '../hooks/useLocalStorage';
+import Login from './Login';
+
+function App() {
+  const [id, setId] = useLocalStorage('id');
+
+  return (
+    <>
+      {id}
+      <Login onIdSubmit={setId} />
+    </>
+  );
+}
+
+export default App;
